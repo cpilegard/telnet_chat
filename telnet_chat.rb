0c370b687de8ed9333cf374844e2725a9515c1b3
Cole Pilegard <c.pilegard@gmail.com>
2013-07-04 16:59:24 -0700

    added private messaging

--- telnet_chat.rb
+++ telnet_chat.rb
@@ -4,11 +4,12 @@ require 'socket'
 class Client
   @@next_color_holder = rand(6) #start with random color each time
 
-  attr_reader :socket, :name
+  attr_reader :socket, :name, :original_name
 
   def initialize(socket, name)
     @socket = socket
 
+    @original_name = name
     col = @@next_color_holder % 6 + 1
     @@next_color_holder += 1
     @name = "\033[3#{col}m#{name}\033[0m"
@@ -21,8 +22,11 @@ class ChatServer
     @tcpserver = TCPServer.new(port)
     @connections = []
     @people = []
+    @currently_logged_in = []
     @connections << @tcpserver
     puts "Server started..."
+
+    @commands = "COMMANDS: -help, -exit, -users, -pm [name] [text]\n"
   end
 
   def add_connection
@@ -37,20 +41,21 @@ class ChatServer
     @people << new_person
 
     new_connection.write("\nWelcome, #{name}!\n")
-    new_connection.write("--type 'exit' to close--\n")
+    new_connection.write("#{@commands}\n")
     new_connection.write("\n> ")
     distribute_message("#{new_person.name} has joined\n", @connections[0])
 
-    currently_logged_in = []
+    # distribute_message("Users: [#{currently_logged_in.join(", ")}]\n", @connections[0])
+  end
+
+  def currently_logged_in
+    people = []
     @connections.each do |connection|
-      @people.each do |person|
-        if connection == person.socket
-          currently_logged_in << person.name
-        end
+      unless connection == @tcpserver
+        people << (get_person_from_socket(connection)).name
       end
     end
-
-    distribute_message("Users: [#{currently_logged_in.join(", ")}]\n", @connections[0])
+    people.join(", ")
   end
 
   def distribute_message(message, sender)
@@ -64,17 +69,25 @@ class ChatServer
       if sender_socket == @tcpserver
         name = "\033[4mserver\033[0m"
       else
-        name = get_name(sender_socket)
+        name = get_person_from_socket(sender_socket).name
       end
       socket.write("#{name}:  #{message}")
       socket.write("> ")
     end
   end
 
-  def get_name(socket)
+  def get_person_from_socket(socket)
     @people.each do |person|
       if person.socket == socket
-        return person.name
+        return person
+      end
+    end
+  end
+
+  def get_person_from_name(name)
+    @people.each do |person|
+      if person.original_name == name
+        return person
       end
     end
   end
@@ -90,9 +103,22 @@ class ChatServer
             add_connection
           else
             msg = socket.gets
-            if msg == "exit\r\n"
+            if msg == "-exit\r\n"
               @connections.delete(socket)
+              leaving = get_person_from_socket(socket)
+              @people.delete(leaving)
               socket.close
+              distribute_message("#{leaving.name} has left\n", @connections[0])
+            elsif msg == "-help\r\n"
+              send_message(socket, @commands, @connections[0])
+            elsif msg == "-users\r\n"
+              send_message(socket, "Users: [#{currently_logged_in}]\n", @connections[0])
+            elsif msg.split(' ', 3)[0] == "-pm"
+              target = get_person_from_name(msg.split(' ', 3)[1])
+              if target != nil
+                send_message(target.socket, "[private] " + msg.split(' ', 3)[2], socket)
+                socket.write("> ")
+              end
             else
               distribute_message(msg, socket)
             end
